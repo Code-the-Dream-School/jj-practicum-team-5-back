06ef8c42e1b3ed8eabd047602b3eaa051610b85c
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-09-11 11:44:40 -0400

    add port

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const start = async () => {
     });
     console.log('Connected to MongoDB');
 
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0", () => {
       console.log(`Server is listening on port: ${PORT}`);
     });
   } catch (err) {
